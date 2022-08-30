c57f10ff6b5380774a7621c64322601c72561272
Sridhar Vadlamani <v.sridhar.sreenivas@gmail.com>
2022-08-29 17:41:20 -0700

    Bump version to 0.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="octohook",
-    version="0.9.0",
+    version="0.10.0",
     author="Sridhar Vadlamani",
     author_email="v.sridhar.sreenivas@gmail.com",
     description="Typed interactions with Github Webhooks",
